2eb08ce1a10b053eab5a4e19a1b1814a81854fe2
Hugo Dueñas <hugo.e.duenas@gmail.com>
2015-01-08 23:16:46 -0600

    corrections to plot1

--- plot1.R
+++ plot1.R
@@ -3,5 +3,5 @@ data <- read.csv(file = "household_power_consumption.txt", sep = ';', na.strings
 data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"), ]
 
 png(filename = "plot1.png", width = 480, height = 480)
-hist(data$Global_active_power, col = "red", main = "Global Active Power" , xlab = "Global Active Power")
+hist(data$Global_active_power, col = "red", main = "Global Active Power" , xlab = "Global Active Power (kilowatts)")
 dev.off()
