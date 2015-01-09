06d1f271bd9883fa728a7f8a9ea24c0376ad4b68
Hugo Dueñas <hugo.e.duenas@gmail.com>
2015-01-08 22:47:21 -0600

    plot1 done

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data <- read.csv(file = "household_power_consumption.txt", sep = ';', na.strings=c("?"), stringsAsFactors=FALSE)
+
+data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"), ]
+
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power" , xlab = "Global Active Power")
+dev.off()
